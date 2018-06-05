b0795d5f8b04ce0f40772f56456cb9ef0e8c9176
Nils Hoffmann <nils.hoffmann@isas.de>
2018-06-05 14:17:27 +0200

    Tests now write reports.

--- src/test/java/de/isas/lipidomics/palinom/RunCucumberTest.java
+++ src/test/java/de/isas/lipidomics/palinom/RunCucumberTest.java
@@ -5,6 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"html:target/cucumber-html-report",
+    "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
+    "usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml"})
 public class RunCucumberTest {
 }
