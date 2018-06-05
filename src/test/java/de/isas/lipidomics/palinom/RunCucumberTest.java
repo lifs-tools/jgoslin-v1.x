af3703f67f74a745508103502cb40f708ede6570
Nils Hoffmann <nils.hoffmann@isas.de>
2018-06-05 14:19:44 +0200

    Removed usage report.

--- src/test/java/de/isas/lipidomics/palinom/RunCucumberTest.java
+++ src/test/java/de/isas/lipidomics/palinom/RunCucumberTest.java
@@ -7,6 +7,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"html:target/cucumber-html-report",
     "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
-    "usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml"})
+    "junit:target/cucumber-results.xml"})
 public class RunCucumberTest {
 }
